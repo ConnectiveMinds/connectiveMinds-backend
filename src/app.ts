528e064476d4a63ae45078dfb2d8dd241682cd01
Sudip8486 <sudeepbhattarai1792@gmail.com>
2023-08-25 17:27:46 +0545

    dbConfiged

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express, {Express} from "express";
+import mongoose,{ConnectOptions} from "mongoose";
+import { dbConfig } from "./config/dbConfig";
+
 const app :Express= express();
 
-
-
+//mongooes database
+mongoose.set("strictQuery", true);
+ mongoose.connect(dbConfig.uri! ,dbConfig.options as ConnectOptions).then(()=>{
+    console.log("Database Connected");
+ }).catch ((error)=>{
+    console.log(error);
+ })
 
 export {app};
---
+++ src/modules/user/v1/model/user.model.ts
@@ -0,0 +1,11 @@
+import mongoose, { model } from "mongoose";
+import { IUser } from "../interface";
+
+
+
+const UserSchema = new mongoose.Schema <IUser>(
+    {
+        
+    }
+)
+
