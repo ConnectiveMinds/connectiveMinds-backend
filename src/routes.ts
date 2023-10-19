d2c7c477dce5abe0e661f8906784ce848e6e3698
Sudip8486 <sudeepbhattarai1792@gmail.com>
2023-10-19 18:01:53 +0545

    Minor Updates

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
-import {Router} from "express";
+import { Router } from "express";
 
 import { userauth } from "./modules/user/v1";
+import { idearouter } from "./modules/ideas/v1/routes/idea.routes";
+export const router = Router();
 
-export const router =Router();
-
-router.use("/v1/user",userauth);
+router.use("/v1/user", userauth);
+router.use("/v1/idea", idearouter);
