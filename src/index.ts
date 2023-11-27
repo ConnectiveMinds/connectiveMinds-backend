cae0e8d16377272d66af99e51fae29c1e9df06f5
Sudip8486 <sudeepbhattarai1792@gmail.com>
2023-11-27 09:42:26 +0545

    Chat:Backend partially completed

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-
-import { Router } from "express";
-import {app} from "./app"
-import { idearouter } from "./modules/ideas/v1/routes/idea.routes";
+import { app } from "./app";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.teamId).emit("receive_message", data);
+  });
+});
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT,()=>{
-    console.log(`Server Started at http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server Started at http://localhost:${PORT}`);
 });
